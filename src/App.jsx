a89f81b9c6facec5e3c67d0b1098de057828bbc2
Alejandro León <juan.aleon2001@gmail.com>
2023-11-11 21:20:43 -0500

    new: added product info path

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './views/Home';
 import Login from './views/Login';
 import SignUp from './views/SignUp';
+import Product from './views/Product';
 import { CartProvider } from './context/CartContext';
 import { AuthProvider } from './context/AuthContext';
 import { ProtectedRoute } from './routes';
@@ -19,11 +20,10 @@ function App() {
               <Route path='/login' element={<Login />} />
               <Route path='/signup' element={<SignUp />} />
               <Route path='/cart' element={<h1>Cart</h1>} />
-
+              <Route path='/product/:id' element={<Product />} />
               <Route element={<ProtectedRoute />}>
                 <Route path='/profile' element={<h1>User</h1>} />
               </Route>
-
               <Route
                 path='*'
                 element={
