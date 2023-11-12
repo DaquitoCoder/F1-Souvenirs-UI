e72e0e04b8b45c0524bbfabfc6705d1bb26f7d16
Alejandro León <juan.aleon2001@gmail.com>
2023-11-11 22:08:07 -0500

    Refactor product view component to include cart functionality

--- src/views/Product.jsx
+++ src/views/Product.jsx
@@ -2,14 +2,19 @@ import { useParams } from 'react-router-dom';
 import SimplePage from '../components/SimplePage';
 import { getProductById } from '../hooks/products/products';
 import { useState, useEffect } from 'react';
-import Container from '../components/Container';
 import Button from '../components/Button';
+import { IconShoppingCart, IconShoppingCartOff } from '@tabler/icons-react';
 import Typography from '../components/Typography';
+import { useCart } from '../context/CartContext';
 
 const Product = () => {
   const { id } = useParams();
 
   const [data, setData] = useState({});
+
+  const { removeFromCart, addToCart, checkProductInCart } = useCart();
+
+  const isProductInCart = checkProductInCart(data);
 
   const fetchData = async () => {
     const res = await getProductById(id);
@@ -24,36 +29,59 @@ const Product = () => {
   return (
     <SimplePage>
       <div className='bg-white flex flex-col items-center justify-center py-12'>
-        <Container className='border rounded-md border-gray-400 flex items-center justify-center flex-col md:flex-row mx-8'>
-          <div className='card-image p-1.5 bg-gray-400 md:w-1/2 rounded-md'>
-            <img src={data.image} alt={data.name} className='rounded-md' />
+        <div className='border rounded-md border-gray-400 flex items-center justify-center flex-col lg:flex-row mx-24 w-[calc(100%-6rem)]'>
+          <div className='card-image p-1.5 md:w-1/2 h-max rounded-md'>
+            <img
+              src={data.image}
+              alt={data.name}
+              className='rounded-md mx-auto'
+            />
           </div>
-          <div className='card-body w-auto md:w-1/2 p-1.5'>
-            <div className='flex justify-around items-center gap-2'>
+          <div className='card-body lg:w-1/2 p-1.5'>
+            <div className='flex flex-col gap-2'>
               <Typography variant='h3' className='text-center'>
                 {data.name}
               </Typography>
-              <Typography variant='h3' className='text-center text-bold'>
-                ${data.price}
+              <Typography variant='p' className='text-center'>
+                Calificaciones: ☆☆☆☆☆
+              </Typography>
+              <Typography variant='p' className='text-center'>
+                <strong>${data.price}</strong>
               </Typography>
             </div>
-            <div className='card-description mx-8 mt-2 text-justify'>{data.description}</div>
-            <div className='card-button flex items-center justify-center gap-4 my-4'>
+            <div className='card-description mx-4 md:mx-8 mt-2 text-justify'>
+              {data.description}
+            </div>
+            <div className='card-button flex flex-col md:flex-row items-center justify-center gap-4 my-4'>
               <Button
                 variant='danger'
                 className='text-white px-6 h-12 uppercase tracking-wider'
               >
                 Comprar ahora
               </Button>
+
               <Button
-                variant='light'
-                className='border border-gray-500 px-6 h-12 uppercase tracking-wider'
+                type='button'
+                variant={isProductInCart ? 'danger' : 'light'}
+                className={`border border-gray-500 px-6 h-12 uppercase tracking-wider ${
+                  isProductInCart ? 'bg-red-700 text-white' : ''
+                }`}
+                icon={
+                  !isProductInCart ? (
+                    <IconShoppingCart size={24} />
+                  ) : (
+                    <IconShoppingCartOff size={24} />
+                  )
+                }
+                onClick={() => {
+                  isProductInCart ? removeFromCart(data) : addToCart(data);
+                }}
               >
-                Añadir al carrito
+                {isProductInCart ? 'Quitar del carrito' : 'Agregar al carrito'}
               </Button>
             </div>
           </div>
-        </Container>
+        </div>
       </div>
     </SimplePage>
   );
