07a9ec51d7db8758c1a4749ac9362ff2aee2c615
Alejandro León <juan.aleon2001@gmail.com>
2023-11-11 21:22:20 -0500

    Add getProductById function to products.js

--- src/hooks/products/products.js
+++ src/hooks/products/products.js
@@ -1,3 +1,5 @@
 import axios from '../axios';
 
 export const getProductsList = async () => axios.get(`/categories-top`);
+
+export const getProductById = async (id) => axios.get(`/products/${id}`);
