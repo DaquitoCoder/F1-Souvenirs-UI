fda80b5c0a5fc3bf8cf2f6ab950da6d768bfd184
Alejandro León <juan.aleon2001@gmail.com>
2023-11-11 21:21:37 -0500

    new: adjusted styles

--- src/components/Alert.jsx
+++ src/components/Alert.jsx
@@ -1,12 +1,10 @@
 const Alert = ({ message }) => {
   return (
-    <div className='text-center py-4 lg:px-4'>
-      <div
-        className='p-2 bg-red-700 items-center text-white leading-none lg:rounded-full flex lg:inline-flex'
-        role='alert'
-      >
-        <span className='font-semibold text-left flex-auto'>{message}</span>
-      </div>
+    <div
+      className='mb-2 p-4 bg-red-700 items-center text-white leading-none lg:rounded-full flex lg:inline-flex text-center'
+      role='alert'
+    >
+      <span className='font-semibold text-left flex-auto'>{message}</span>
     </div>
   );
 };
--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,9 +1,13 @@
 const Button = (props) => {
   const classNames = {
-    primary: 'bg-blue-500 hover:bg-blue-700 rounded',
-    secondary: 'bg-gray-500 hover:bg-gray-700 rounded',
-    success: 'bg-green-500 hover:bg-green-700 rounded',
-    danger: 'bg-[#B40500] hover:bg-red-700 rounded',
+    primary:
+      'bg-blue-500 hover:bg-blue-700 rounded focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500 active:bg-blue-800',
+    secondary:
+      'bg-gray-500 hover:bg-gray-700 rounded focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-500 active:bg-gray-800',
+    success:
+      'bg-green-500 hover:bg-green-700 rounded focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-green-500 active:bg-green-800',
+    danger:
+      'bg-[#B40500] hover:bg-red-700 rounded focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-red-500 active:bg-red-800',
     warning: 'bg-yellow-500 hover:bg-yellow-700 rounded',
     info: 'bg-blue-500 hover:bg-blue-700 rounded',
     light: 'bg-gray-100 hover:bg-gray-200 rounded',
