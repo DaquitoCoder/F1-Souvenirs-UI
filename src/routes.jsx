b8469055d8040b6f3beacf8d579687f2ff7676e4
Alejandro León <juan.aleon2001@gmail.com>
2023-11-11 21:20:48 -0500

    Remove console log statement in ProtectedRoute component

--- src/routes.jsx
+++ src/routes.jsx
@@ -4,8 +4,6 @@ import { useAuth } from './context/AuthContext';
 export const ProtectedRoute = () => {
   const { isAuthenticated, loading } = useAuth();
 
-  console.log('isauh', isAuthenticated, 'islo', loading);
-
   if (loading) return <h1>Loading...</h1>;
   if (!isAuthenticated && !loading) return <Navigate to='/login' replace />;
   return <Outlet />;
