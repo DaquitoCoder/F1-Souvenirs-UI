0de08a6c30dbace91f0032541f3ddb8b1bca0e5e
Alejandro León <juan.aleon2001@gmail.com>
2023-11-15 23:09:46 -0500

    Add Products view with fetching and displaying all products

---
+++ src/views/Products.jsx
@@ -0,0 +1,47 @@
+import { useEffect, useState } from 'react';
+import SimplePage from '../components/SimplePage';
+import Typography from '../components/Typography';
+import { getAllProducts } from '../hooks/products/products';
+import Card from '../components/Card';
+
+const Products = () => {
+  const [dataProducts, setDataProducts] = useState([]);
+
+  const fetchData = async () => {
+    try {
+      const res = await getAllProducts();
+      console.log(res);
+      setDataProducts(res.data);
+    } catch (error) {
+      console.log(error);
+      setDataProducts(error.response.data.errors);
+    }
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  return (
+    <SimplePage>
+      <div className='bg-white flex flex-col items-center justify-center py-24'>
+        <div className='products-title mb-8'>
+          <Typography variant='h1'>Todos los productos</Typography>
+        </div>
+        <div className='products-category-items mx- grid gap-4 grid-cols-1 md:grid-cols-2 lg:grid-cols-4 xl:grid-cols-5'>
+          {dataProducts && dataProducts.length > 0 ? (
+            <>
+              {dataProducts.map((product) => (
+                <Card key={product._id} item={product} storeButtons />
+              ))}
+            </>
+          ) : (
+            <p>No hay productos</p>
+          )}
+        </div>
+      </div>
+    </SimplePage>
+  );
+};
+
+export default Products;
