328f2c2e21393e780bc38951a6df0979504ab925
Alejandro León <juan.aleon2001@gmail.com>
2023-11-16 00:09:22 -0500

    Add getAllProductsBySeller function to products.js

--- src/hooks/products/products.js
+++ src/hooks/products/products.js
@@ -5,3 +5,6 @@ export const getProductsList = async () => axios.get(`/categories-top`);
 export const getProductById = async (id) => axios.get(`/products/${id}`);
 
 export const getAllProducts = async () => axios.get(`/products`);
+
+export const getAllProductsBySeller = async (id) =>
+  axios.get(`/products/seller/${id}`);
