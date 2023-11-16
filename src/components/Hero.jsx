fa55a26b31d55d50d69ed1e55a3a5bc019ae7e45
Alejandro León <juan.aleon2001@gmail.com>
2023-11-15 23:09:11 -0500

    Add link to product page in Hero component

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,3 +1,4 @@
+import { Link } from 'react-router-dom';
 import '../index.css';
 import Button from './Button';
 import Typography from './Typography';
@@ -9,9 +10,11 @@ const Hero = () => {
         <Typography variant='h2' className='mb-2'>
           Temporada 2023 - Ofertas
         </Typography>
-        <Button type='button' variant='danger' className='p-2'>
-          Mira las ofertas
-        </Button>
+        <Link to='/products'>
+          <Button type='button' variant='danger' className='px-4 py-2'>
+            Mira las ofertas
+          </Button>
+        </Link>
       </div>
     </div>
   );
