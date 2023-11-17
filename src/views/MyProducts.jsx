d6481ca1376ae925cb771d88a085b1716f18f968
Alejandro León <juan.aleon2001@gmail.com>
2023-11-17 00:25:24 -0500

    Add new features to MyProducts view

--- src/views/MyProducts.jsx
+++ src/views/MyProducts.jsx
@@ -3,8 +3,11 @@ import SimplePage from '../components/SimplePage';
 import Typography from '../components/Typography';
 import { getAllProductsBySeller } from '../hooks/products/products';
 import Card from '../components/Card';
-import { useAuth } from '../context/AuthContext';
+import { IconPlus } from '@tabler/icons-react';
 import ProductModal from '../components/ProductModal';
+import Button from '../components/Button';
+import Container from '../components/Container';
+import { Link } from 'react-router-dom';
 
 const MyProducts = () => {
   const [dataProducts, setDataProducts] = useState([]);
@@ -21,43 +24,52 @@ const MyProducts = () => {
     setIsModalOpen(true);
   };
 
-  const { user } = useAuth();
-
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const res = await getAllProductsBySeller(user.id);
+        const res = await getAllProductsBySeller();
         setDataProducts(res.data);
       } catch (error) {
         setDataProducts(error.response.data.errors);
       }
     };
     fetchData();
-  }, [user.id]);
+  }, []);
 
   return (
     <SimplePage>
-      <div className='bg-white flex flex-col items-center justify-center py-24'>
-        <div className='products-title mb-8'>
-          <Typography variant='h1'>Mis productos</Typography>
+      <Container className='bg-white pb-4'>
+        <div className='products-category mx-6 py-6 lg:mx-24'>
+          <div className='products-category-title flex flex-wrap justify-between mb-4'>
+            <Typography variant='h1'>Mis productos</Typography>
+            <Link to='/profile/new-product' className='flex'>
+              <Button
+                variant='success'
+                icon={<IconPlus size={24} />}
+                className='text-white p-2'
+              >
+                Nuevo producto
+              </Button>
+            </Link>
+          </div>
+          <div className='products-category-items grid gap-4 grid-cols-1 md:grid-cols-2 lg:grid-cols-4 xl:grid-cols-5'>
+            {dataProducts && dataProducts.length > 0 ? (
+              <>
+                {dataProducts.map((product) => (
+                  <Card
+                    key={product._id}
+                    item={product}
+                    editButtons
+                    buttonDeleteAction={openModal}
+                  />
+                ))}
+              </>
+            ) : (
+              <p>No hay productos</p>
+            )}
+          </div>
         </div>
-        <div className='products-category-items grid gap-4 grid-cols-1 md:grid-cols-2 lg:grid-cols-4 xl:grid-cols-5'>
-          {dataProducts && dataProducts.length > 0 ? (
-            <>
-              {dataProducts.map((product) => (
-                <Card
-                  key={product._id}
-                  item={product}
-                  editButtons
-                  buttonDeleteAction={openModal}
-                />
-              ))}
-            </>
-          ) : (
-            <p>No hay productos</p>
-          )}
-        </div>
-      </div>
+      </Container>
       <ProductModal
         onClose={closeModal}
         isOpen={isModalOpen}
