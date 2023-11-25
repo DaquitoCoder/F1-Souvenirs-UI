e859464c53ddeff589f4a788d53dcd20a00715e3
Alejandro León <juan.aleon2001@gmail.com>
2023-11-25 18:27:32 -0500

    Add MyOrders component for displaying user orders

---
+++ src/views/MyOrders.jsx
@@ -0,0 +1,100 @@
+import { getOrders } from '../hooks/products/orders';
+import { useAuth } from '../context/AuthContext';
+import { useEffect, useState } from 'react';
+import SimplePage from '../components/SimplePage';
+import Typography from '../components/Typography';
+import dayjs from 'dayjs';
+
+const MyOrders = () => {
+  const { user } = useAuth();
+
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const res = await getOrders(user.id);
+        setData(res.data);
+      } catch (error) {
+        setData([error.response.data.message]);
+      }
+    };
+    fetchData();
+  }, [user.id]);
+
+  return (
+    <SimplePage>
+      <div className='bg-white flex flex-col items-center justify-center py-8'>
+        <div className='orders-title mb-6'>
+          <Typography variant='h1'>Mis ordenes</Typography>
+        </div>
+        <div className='orders-cards'>
+          {data && data.length > 0 ? (
+            <>
+              {data.map((order) => (
+                <div
+                  className='order-card border border-radius rounded-md p-4 mb-4 flex flex-col gap-4'
+                  key={order._id}
+                >
+                  <div className='order-card-title'>
+                    <Typography variant='h2'>Orden: {order._id}</Typography>
+                  </div>
+                  <div className='order-card-header'>
+                    <Typography variant='h3' className='text-center mb-2'>
+                      Fecha de compra:{' '}
+                      {dayjs(order.createdAt)
+                        .locale('es')
+                        .format('DD/MM/YYYY, hh:mm A')}
+                    </Typography>
+                  </div>
+                  <div className='order-card-body'>
+                    <Typography variant='h3' className='text-center mb-2'>
+                      Productos
+                    </Typography>
+                    <div className='order-card-body-products'>
+                      <table className='table-auto w-full border border-black text-center'>
+                        <thead className='border border-black'>
+                          <tr>
+                            <th className='border border-black'>Nombre</th>
+                            <th className='border border-black'>Precio</th>
+                            <th className='border border-black'>Cantidad</th>
+                          </tr>
+                        </thead>
+                        <tbody>
+                          {order.items.map((product) => (
+                            <tr key={product._id}>
+                              <td className='border border-black'>
+                                {product.product.name}
+                              </td>
+                              <td className='border border-black'>
+                                ${product.product.price}
+                              </td>
+                              <td className='border border-black'>
+                                {product.quantity}
+                              </td>
+                            </tr>
+                          ))}
+                        </tbody>
+                      </table>
+                    </div>
+                  </div>
+                  <div className='order-card-footer'>
+                    <Typography variant='h3'>
+                      Total: ${order.totalAmount}
+                    </Typography>
+                  </div>
+                </div>
+              ))}
+            </>
+          ) : (
+            <Typography variant='h3' className='text-center my-4'>
+              No tienes ordenes en este momento.
+            </Typography>
+          )}
+        </div>
+      </div>
+    </SimplePage>
+  );
+};
+
+export default MyOrders;
