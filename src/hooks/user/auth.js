354dda285b6d9d7e21e7c6b3225213c0f6bd936b
Alejandro León <juan.aleon2001@gmail.com>
2023-11-29 00:55:59 -0500

    Refactor auth.js file

--- src/hooks/user/auth.js
+++ src/hooks/user/auth.js
@@ -4,11 +4,7 @@ export const registerRequest = async (user) => axios.post(`/signup`, user);
 
 export const loginRequest = async (user) => axios.post(`/login`, user);
 
-export const verifyTokenRequest = async (token) =>
-  axios.get(`/verify`, {
-    headers: {
-      Authorization: `Bearer ${token}`,
-    },
-  });
+export const verifyTokenRequest = async () => axios.get(`/verify`);
 
-export const editUserDataRequest = async (user) => axios.put(`/user/${user.id}`, user);
+export const editUserDataRequest = async (user) =>
+  axios.put(`/user/${user._id}`, user);
